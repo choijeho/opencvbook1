22979feb1725a5e406cacf2fc60e0dd43c2304c9
choijeho <choijeho@naver.com>
2023-06-30 13:36:05 +0900

    typo correction

--- 02.interface/2-1.img_show.py
+++ 02.interface/2-1.img_show.py
@@ -16,7 +16,7 @@ img = cv2.imread(img_file)  # 이미지를 읽어서 img 변수에 할당 ---②
 if img is not None:
   cv2.imshow('IMG', img)   # 읽은 이미지를 화면에 표시      --- ③
   cv2.waitKey()           # 키가 입력될 때 까지 대기      --- ④
-  cv2.
+  cv2.destroyAllWindows()
 else:
     print('No image file.')
     
